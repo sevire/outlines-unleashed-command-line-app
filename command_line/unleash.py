5d8b0ff0e512aa06fd16c661a213c96320b874cb
Thomas Gaylard Admin <tbg-beanstalk@genonline.co.uk>
2021-03-28 21:21:44 +0100

    First quick and dirty cobbling together of working command line: - Using a data structure rather than json file for data node descriptor (DND) - It's a bit muddy exactly what format the DND should be in. - But it basically works!

--- command_line/unleash.py
+++ command_line/unleash.py
@@ -3,6 +3,8 @@ from outlines_unleashed.data_node_specifier import DataNodeSpecifier
 from outlines_unleashed.unleashed_outline import UnleashedOutline
 import sys
 
+from tests.test_resources.test_data import data_node_specifier_test_01
+
 if __name__ == "__main__":
     num_arguments = len(sys.argv)
     expected_num_arguments = 2  # Note command line arguments will be one more as filename is first
@@ -12,8 +14,9 @@ if __name__ == "__main__":
         # opml_path = sys.argv[1]
         # outline_specifier_json = sys.argv[2]
 
-        opml_path = "/Users/thomasdeveloper/Documents/Projects/outlines-unleashed/tests/test_resources/input_files/data_node_descriptor/custom_json_test_descriptors.opml"
-        outline_specifier_json = "/Users/thomasdeveloper/Documents/Projects/outlines-unleashed/tests/test_resources/input_files/data_node_descriptor/custom_json_test_descriptors_generic_levels.json"
+        opml_path = "/Users/thomasdeveloper/Documents/Projects/outlines_unleashed_command_line/tests/test_resources/opml_data_extraction_test_01.opml"
+        # outline_specifier_json = "/Users/thomasdeveloper/Documents/Projects/outlines_unleashed_command_line/tests/test_resources/opml_data_extraction_test_01a.json"
+        outline_specifier_data = data_node_specifier_test_01
 
         outline = Outline.from_opml(opml_path)
         print("Successfully read outline, unleashing...")
@@ -29,7 +32,10 @@ if __name__ == "__main__":
         data_node_list_index = data_node_generators[0]['data_node_list_index']
         data_node = unleashed_outline.list_unleashed_nodes()[data_node_list_index].node()
 
-        data_node_specifier = DataNodeSpecifier.from_json(outline_specifier_json)
+        # with open(outline_specifier_json, 'r') as json_file:
+        #     json_string = json_file.read().replace('\n', '')
+
+        data_node_specifier = DataNodeSpecifier(outline_specifier_data)
         extracted_data_table = data_node_specifier.extract_data_node_dispatch(data_node)
 
         pass
